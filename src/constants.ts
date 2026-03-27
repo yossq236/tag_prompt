7e567796f0448d3ad70c065560be6ad087395625
yossq236 <yf0659@gmail.com>
2026-03-27 15:32:27 +0900

    fix: regex quantifier for digit length

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const HIGHLIGHT_TOKENS = {
     BLOCK_COMMENT_BEGIN: '/*',
     BLOCK_COMMENT_END: '*/',
     ESCAPED_PARENTHESIS: /\\[()]/,
-    WEIGHT: /:[0-9\.]/,
+    WEIGHT: /:[0-9\.]+/,
     CHARS: /[<>()&"',\n]/,
 };
 export const HEADER_TOKENS = {
